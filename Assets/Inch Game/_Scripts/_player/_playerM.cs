f8b4c3b28497daff64cfb608c869dffc19aabeff
Dhirajlal Vishwakarma <47329713+dhiraj7894@users.noreply.github.com>
2021-07-17 22:21:42 +0530

    Update

---
+++ Assets/Inch Game/_Scripts/_player/_playerM.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _playerM : MonoBehaviour
+{
+    public Transform WallRays;
+    public GameObject TargetedWall;
+
+    public float movementSpeed = 2;
+    public float wDDistance; //wall ditaction distance
+    public float dFTWall; //Distance From Target Wall
+
+
+    public LayerMask wall;
+
+    void Update()
+    {
+        move();
+        WallChecker();
+        distanceCalculator();
+    }
+
+    void move()
+    {
+        transform.Translate(transform.forward * movementSpeed * Time.deltaTime);
+    }
+
+    void WallChecker()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(WallRays.position, WallRays.forward,out hit, wDDistance, wall))
+        {
+            TargetedWall = hit.transform.gameObject;
+        }
+    }
+
+    void distanceCalculator()
+    {
+        if (TargetedWall == null)
+            return;
+        dFTWall = TargetedWall.transform.position.z - transform.position.z;
+
+        if (dFTWall <= -2)
+        {
+            TargetedWall.GetComponent<Wall>().playerPassed = true;
+        }
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.black;
+        Gizmos.DrawLine(WallRays.position, WallRays.forward * 10);
+    }
+}
---
+++ Assets/Inch Game/_Scripts/ch_04/bodyPartMV.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bodyPartMV : MonoBehaviour
+{
+    private SpriteRenderer sIndicator;
+    private Vector3 mOffset;
+    private float mZCoord;
+
+    public bool isMoving = false;
+    private void Start()
+    {
+        sIndicator = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        sIndicator.color = new Color (1,0,0,0.6f);
+    }
+    private void Update()
+    {
+        if (isMoving)
+        {
+            transform.position = GetMouseAsWorldPoint() + mOffset;
+        }
+    }
+    void OnMouseDown()
+    {
+        isMoving = true;
+        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        // Store offset = gameobject world pos - mouse world pos
+        mOffset = gameObject.transform.position - GetMouseAsWorldPoint();
+        sIndicator.color = new Color(0, 1, 0, 0.6f);
+    }
+    private void OnMouseUp()
+    {
+        isMoving = false;
+        sIndicator.color = new Color(1,0,0,0.6f);
+    }
+    private Vector3 GetMouseAsWorldPoint()
+    {
+        // Pixel coordinates of mouse (x,y)
+
+        Vector3 mousePoint = Input.mousePosition;
+
+        // z coordinate of game object on screen
+
+        mousePoint.z = mZCoord;
+        // Convert it to world points
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+}
